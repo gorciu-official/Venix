357676b851fb9374c9d81857e086a029b34c573d
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2025-06-02 15:34:14 +0000

    [drivers] minor video fixes

--- src/drivers/video/general.c
+++ src/drivers/video/general.c
@@ -6,9 +6,14 @@ void print(const char* str) {
     vga_print(str);
 }
 
+void println(const char* str) {
+    print(str);
+    print("\n");
+}
+
 bool printk(const char* str) {
 #ifndef __VENIX_DISABLE_KPRINT
-    print(str);
+    println(str);
     return true;
 #else
     return false;
--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 #include <types.h>
+#include <drivers/video.h>
 
 int main() {
-    vga_print("\x1b[31;47mHello \x1b[34mWorld!\n");
-    vga_print("This is \x1b[32mgreen\x1b[0m and this \x1b[41mred background\x1b[0m.\n");
-    vga_print("\x1b[10;10HCursor moved here!\n");
+    print('');
     return 0;
 }
